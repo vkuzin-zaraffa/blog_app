4073b79f2ca41f5fba056e4252bf4fa03103c55e
Valerii <vkuzin@zaraffasoft.com>
2018-09-21 00:28:10 +0300

    Create Article Succesfulle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,23 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created"
+    else
+      flash[:error] = "Article not created"
+    end
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
